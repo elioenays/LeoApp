c0dcaad43bdf221ca8d0beb6dd76f2f23a511337
Elioenay <elionay407@gmail.com>
2022-05-19 08:31:36 -0300

    fix details volunt

--- components/DetailsVoluntary.jsx
+++ components/DetailsVoluntary.jsx
@@ -11,7 +11,7 @@ const DetailsVoluntary = ({ voluntary }) => {
         justifyContent: 'space-between',
         alignItems: 'center',
         marginVertical: SIZES.base,
-        paddingHorizontal: SIZES.base,
+        paddingHorizontal: SIZES.base * 2,
       }}
     >
       <Image
