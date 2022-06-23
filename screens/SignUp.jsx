d09a2aa7f64cecabac84a4f24ca57f770cbdc5eb
Elioenay Silva <elionay407@gmail.com>
2022-06-22 23:52:40 -0300

    init

--- screens/SignUp.jsx
+++ screens/SignUp.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
+import {
+  Image,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { FocusedStatusBar, MainRectButton } from '../components'
 import { styles } from '../constants'
-import { View, TextInput, Text, Image, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
-import { MainRectButton, FocusedStatusBar } from '../components'
 
 const SignUp = () => {
-  
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const cadastro = () => {
     //alert(name);
@@ -18,41 +24,39 @@ const SignUp = () => {
 
   return (
     <KeyboardAvoidingView style={styles.container}>
-
-      <View style={ styles.containerLogo }>
-        <Image style={styles.logo} source= {require('../assets/logo_leo.png')} />
-
+      <View style={styles.containerLogo}>
+        <Image style={styles.logo} source={require('../assets/logo_leo.png')} />
       </View>
 
-      <View style={ styles.container }>
-        <FocusedStatusBar style="auto" />
+      <View style={styles.container}>
+        <FocusedStatusBar style='auto' />
 
-        <Text style={ styles.label }>Crie sua conta</Text>
-        <TextInput 
-          name="inputName" 
-          placeholder='Nome' 
-          onChange={text=>setName(text)} 
-          style={ styles.textInput } 
+        <Text style={styles.label}>Crie sua conta</Text>
+        <TextInput
+          name='inputName'
+          placeholder='Nome'
+          onChange={text => setName(text)}
+          style={styles.textInput}
         />
 
-        <TextInput  
-          name="inputEmail" 
-          placeholder='E-mail' 
+        <TextInput
+          name='inputEmail'
+          placeholder='E-mail'
           autoCorrect={false}
-          onChange={text=>setEmail(text)} 
-          style={ styles.textInput } 
+          onChange={text => setEmail(text)}
+          style={styles.textInput}
         />
 
-        <TextInput 
-          secureTextEntry={true} 
-          name="inputPassword" 
-          placeholder='Senha' 
+        <TextInput
+          secureTextEntry={true}
+          name='inputPassword'
+          placeholder='Senha'
           autoCorrect={false}
-          onChange={text=>setPassword(text)} 
-          style={ styles.textInput }
+          onChange={text => setPassword(text)}
+          style={styles.textInput}
         />
 
-        <MainRectButton onPress={()=>cadastro()} />
+        <MainRectButton onPress={() => cadastro()} />
 
         <TouchableOpacity>
           <Text style={styles.loginText}>
